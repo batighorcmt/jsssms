1eca8d730c8464fd72bdd55d92c8cd574844fc3c
Batighor Computers <ourbatighor@gmail.com>
2025-06-11 09:17:09 +0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "batighor_user";
+$password = "@Halim#6713";
 $dbname = "batighor_jss";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
