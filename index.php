3c72c4e4c8c0fc856d024d405857698fa9ad9100
Batighor Computers <ourbatighor@gmail.com>
2025-06-11 09:29:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+
+
 <?php
 // index.php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 if (isset($_SESSION['user'])) {
     header("Location: dashboard.php");
