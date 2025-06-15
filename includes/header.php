d45d034dd0efcbae520a9f44c688b79fee44e431
Batighor Computers <ourbatighor@gmail.com>
2025-06-15 14:08:25 +0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,9 @@ if (session_status() == PHP_SESSION_NONE) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Bootstrap Icons CDN -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
   <script>
     var modal = new bootstrap.Modal(document.getElementById('exampleModal'));
     document.getElementById('openModalBtn').addEventListener('click', function() {
