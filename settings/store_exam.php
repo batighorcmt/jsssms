38b1eddc1f23462ebbc98d368367cc7edb42b538
Batighor Computers <ourbatighor@gmail.com>
2025-07-17 21:19:23 +0600

    Update store_exam.php

--- settings/store_exam.php
+++ settings/store_exam.php
@@ -24,33 +24,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $objective_pass = $_POST['objective_pass'];
     $practical_pass = $_POST['practical_pass'];
 
-for ($i = 0; $i < count($subject_id); $i++) {
-    $total_marks = (int)$creative_marks[$i] + (int)$objective_marks[$i] + (int)$practical_marks[$i];
+    for ($i = 0; $i < count($subject_id); $i++) {
+        $total_marks = (int)$creative_marks[$i] + (int)$objective_marks[$i] + (int)$practical_marks[$i];
 
-    $sql2 = "INSERT INTO exam_subjects (
-                exam_id, subject_id, exam_date, exam_time,
-                creative_marks, objective_marks, practical_marks, total_marks,
-                creative_pass, objective_pass, practical_pass
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $sql2 = "INSERT INTO exam_subjects (
+                    exam_id, subject_id, exam_date, exam_time,
+                    creative_marks, objective_marks, practical_marks, total_marks,
+                    creative_pass, objective_pass, practical_pass
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    $stmt2 = $conn->prepare($sql2);
-    $stmt2->bind_param(
-        "iissiiiiiii",
-        $exam_id,
-        $subject_id[$i],           // subject_id should be an integer
-        $exam_dates[$i],
-        $exam_times[$i],
-        $creative_marks[$i],
-        $objective_marks[$i],
-        $practical_marks[$i],
-        $total_marks,
-        $creative_pass[$i],
-        $objective_pass[$i],
-        $practical_pass[$i]
-    );
-    $stmt2->execute();
-}
-
+        $stmt2 = $conn->prepare($sql2);
+        $stmt2->bind_param(
+            "iissiiiiiii",
+            $exam_id,
+            $subject_ids[$i],
+            $exam_dates[$i],
+            $exam_times[$i],
+            $creative_marks[$i],
+            $objective_marks[$i],
+            $practical_marks[$i],
+            $total_marks,
+            $creative_pass[$i],
+            $objective_pass[$i],
+            $practical_pass[$i]
+        );
+        $stmt2->execute();
+    }
 
     echo "<script>alert('Exam created successfully!'); window.location.href='create_exam.php';</script>";
 } else {
