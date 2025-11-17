c48be3040a3ce7c3c48176153e02b939fe3133ad
Batighor Computers <ourbatighor@gmail.com>
2025-11-17 23:16:41 +0600

    Update register.php

--- api/devices/register.php
+++ api/devices/register.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Registers/updates an FCM device token for a user.
 // Accepts:
 //  - Authorization: Bearer <api_token> (preferred)
